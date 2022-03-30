e11de93741c810633dd325e4e2965f70e42fd690
WiktorrWie <wiktor.wieczorek@interia.pl>
2022-03-30 13:47:02 +0200

    v10
    seed - adding default code snippets

---
+++ app/routes/seed.jsx
@@ -0,0 +1,40 @@
+import { redirect } from "remix";
+import { useLoaderData, Form } from "remix";
+import connectDb from "~/db/connectDb.server";
+
+
+export async function loader() {
+    const db = await connectDb();
+    const numberOfBooks = await db.models.Book.find()
+    return (
+        { books: numberOfBooks.length }
+    )
+}
+
+export async function action() {
+    const db = await connectDb();
+
+    await db.models.Book.deleteMany({});
+
+    await db.models.Book.insertMany([
+        {
+            "title": "HTML form",
+            "description": "some description..."
+        },
+        {
+            "title": "Tailwind",
+            "description": "some description..."
+        }
+    ]);
+    return redirect("/")
+}
+
+export default function Index() {
+    return (
+        <Form method="post">
+            <button type="submit">Add defualt snippets</button>
+        </Form>
+        
+    )
+}
+  
