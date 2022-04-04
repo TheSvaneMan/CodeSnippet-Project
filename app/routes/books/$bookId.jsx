510727eadce6a3505c45e40194f01a29c548e327
WiktorrWie <wiktor.wieczorek@interia.pl>
2022-04-04 11:44:52 +0200

    v21

--- app/routes/books/$bookId.jsx
+++ app/routes/books/$bookId.jsx
@@ -1,13 +1,35 @@
 import { useLoaderData, Link } from "remix";
 import connectDb from "~/db/connectDb.server.js";
+import React, { useRef, useState } from 'react';
 
 export async function loader({ params }) {
   const db = await connectDb();
   return db.models.Book.findById(params.bookId);
 }
 
+
+
 export default function BookPage() {
-  const book = useLoaderData();
+  const books = useLoaderData();
+  const [show, setShow] = useState(null);
+  const [sort, setSort] = useState("");
+
+  let sortedBooks;
+  if (sort == "sortByName") {
+    sortedBooks = [...books].sort(function (a, b) {
+      return a.title.localeCompare(b.title);
+    });;
+  }
+  else if (sort == "showFavorite") {
+    sortedBooks = books.filter(function (book) {
+      return book.favorite == true;
+    })
+  }
+
+  else {
+    sortedBooks = books;
+  }
+
   return (
     <div className="m-6 w-1/2">
           <h2 className="text-lg font-bold mb-3">
