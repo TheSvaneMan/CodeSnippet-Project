e382f9f464e158ff90abcb4c2a40341dff95b301
WiktorrWie <wiktor.wieczorek@interia.pl>
2022-05-04 23:56:35 +0200

    v6
    css, double password verification with error message

--- app/routes/login.jsx
+++ app/routes/login.jsx
@@ -19,16 +19,16 @@ export default function Index() {
       <div className="m-6 w-1/2">
       <h1 className="text-2xl font-bold mb-4">Log in</h1>
       <Form method="post" reloadDocument>
-        <input type="text" name="username" placeholder="username" className="py-1 px-2 rounded-lg"></input>
+        <input type="text" name="username" placeholder="username" className="py-1 px-2 rounded-lg dark:text-neutral-800 focus:outline-orange-400"></input>
         <br />
-        <input type="text" name="password" placeholder="password" className="py-1 px-2 rounded-lg mt-4"></input>
+        <input type="text" name="password" placeholder="password" className="py-1 px-2 rounded-lg mt-4 dark:text-neutral-800 focus:outline-orange-400"></input>
         <br />
         <button type="submit" className="mt-3 mb-2 pr-3 pl-3 pt-0 pb-1 border-2 
                   border-orange-400 bg-neutral-800 text-neutral-50 rounded-3xl
                   hover:bg-orange-400">Log in</button>
           <h1 className="text-red-500"> {loginStatus} </h1>
           No account yet?
-          <Link to="/signup" className="ml-5 hover:text-neutral-800 text-orange-400">
+          <Link to="/signup" className="ml-5 hover:text-neutral-50 text-orange-400">
           Sign up!
           </Link>
         </Form>
--- app/routes/signup.jsx
+++ app/routes/signup.jsx
@@ -8,17 +8,40 @@ export async function action({ request }) {
   const session = await getSession(request.headers.get("Cookie"));
   const db = await connectDb();
 
+  if (form.get("password").trim() !== form.get("repeatPassword").trim()) {
+    return json(
+      { errorMessage: "Passwords are not the same!" },
+      { status: 400 }
+    );
+  }
+
     const hashedPassword = await bcrypt.hash(form.get("password").trim(),10);
-  
+  try {
     let user = await db.models.user.create({ username: form.get("username"), password: hashedPassword });
-    session.set("userID", user._id);
-    return redirect("/snippets", {
-      headers: {
-      "Set-Cookie": await commitSession(session),
-      },
+    if (user) {
+      session.set("userID", user._id);
+      return redirect("/snippets", {
+        headers: {
+          "Set-Cookie": await commitSession(session),
+        },
       });
-  
-}
+    } else {
+      return json(
+        { errorMessage: "User couldn't be created" },
+        { status: 400 }
+      );
+    }
+  } catch (error) {
+      return json(
+        {
+          errorMessage:
+            error.message ??
+            error.errors?.map((error) => error.message).join(", "),
+        },
+        { status: 400 }
+      );
+    }
+};
 
 export default function SingUp() {
   const actionData = useActionData();
@@ -31,29 +54,34 @@ export default function SingUp() {
           name="username"
           id="username"
           placeholder="username"
-          className="py-1 px-2 rounded-lg"
+          className="py-1 px-2 rounded-lg dark:text-neutral-800 focus:outline-orange-400"
         />
-        {actionData?.errors.username && (
-          <p className="text-red-500">{actionData.errors.username.message}</p>
-        )}
         <br />
         <input
-          type="text"
+          type="password"
           name="password"
           id="password"
           placeholder="password"
-          className="py-1 px-2 rounded-lg mt-4"
+          className="py-1 px-2 rounded-lg mt-4 dark:text-neutral-800 focus:outline-orange-400"
         />
-        {actionData?.errors.password && (
-          <p className="text-red-500">{actionData.errors.password.message}</p>
-        )}
+        <br />
+        <input
+          type="password"
+          name="repeatPassword"
+          id="repeatPassword"
+          placeholder="repeat password"
+          className="py-1 px-2 rounded-lg mt-4 dark:text-neutral-800 focus:outline-orange-400"
+        />
         <br />
         <button type="submit" className="mt-3 mb-2 pr-3 pl-3 pt-0 pb-1 border-2 
                   border-orange-400 bg-neutral-800 text-neutral-50 rounded-3xl
                   hover:bg-orange-400">Register</button>
         <br />
+        {actionData?.errorMessage ? (
+        <p className="text-red-500 font-bold my-3">{actionData.errorMessage}</p>
+      ) : null}
         You have an account?
-          <Link to="/login" className="ml-5 hover:text-neutral-800 text-orange-400">
+          <Link to="/login" className="ml-5 hover:text-neutral-50 text-orange-400">
           Log in!
           </Link>
       </Form>
