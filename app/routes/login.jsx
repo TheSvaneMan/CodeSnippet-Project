d6da6b76195314419c47eca83a1fbde902fc6432
Penda Svane <thesvaneman@gmail.com>
2022-05-10 10:49:57 +0200

    (Enhancement) Offline UI and UX - Tier 1
    -> Small update to the login to prevent offline network required actions such as trying to login or signup. Provide an offline UX
    -> Update to root to show app network state (This could look differently of course, it is first version)
    -> Added basic version of sharing code snippet link, could not add feature to send to email provided this branch doesn't have the userId updates.
    
    ** TLDR; Take note of Login page on one possible way to update UI when offline, unfortunately does not update until a reload occuers

--- app/routes/login.jsx
+++ app/routes/login.jsx
@@ -1,5 +1,6 @@
 import { Form, redirect, useActionData, useLoaderData, Link, useCatch, json } from "remix";
 import { getSession, commitSession } from "~/sessions.server";
+import { useState, useEffect } from "react";
 import connectDb from "~/db/connectDb.server";
 import bcrypt from "bcryptjs";
 
@@ -16,6 +17,26 @@ export async function loader({ request }) {
 export default function Index() {
   const { userID } = useLoaderData();
   const loginStatus = useActionData();
+  const [networkState, setNetworkState] = useState();
+  const [showSignUpWarning, setSignUpWarning] = useState(false);
+
+  // App network state
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    // Client
+    if (navigator.onLine) {
+      setNetworkState('online');
+    } else {
+      setNetworkState('offline');
+    }
+  }, []);
+
+  // --------- UX ---------------- //
+  const toggleSignUpWarning = (e) => {
+    e.preventDefault();
+    setSignUpWarning(!showSignUpWarning);
+  }
 
   if (userID != null) {
     return redirect("/snippets");
@@ -27,16 +48,23 @@ export default function Index() {
           <div id="login-input-fields" className='grid grid-cols-1 space-y-5'>
             <input type="text" name="username" placeholder="username" className="py-1 px-2 rounded-lg dark:text-neutral-800 focus:outline-orange-400"></input>
             <input type="text" name="password" placeholder="password" className="py-1 px-2 rounded-lg mt-4 dark:text-neutral-800 focus:outline-orange-400"></input>
-            <button type="submit" className="mt-3 mb-2 pr-3 pl-3 pt-0 pb-1 border-2 
+            {networkState === 'online' ? <button type="submit" className="mt-3 mb-2 pr-3 pl-3 pt-0 pb-1 border-2 
                   border-orange-400 bg-neutral-800 text-neutral-50 rounded-3xl
-                  hover:bg-orange-400">Log in</button>
+                  hover:bg-orange-400">Login</button> : <button type="submit" disabled className="mt-3 mb-2 pr-3 pl-3 pt-0 pb-1 border-2 
+                  border-orange-400 bg-red-800 text-neutral-50 rounded-3xl">Login</button>}
+
           </div>
           <p className="text-red-500 font-bold my-3"> {loginStatus} </p>
           No account yet?
-          <Link to="/signup" className="ml-5 hover:text-neutral-800 dark:hover:text-neutral-50 text-orange-400">
+          {networkState === 'online' ? <Link to="/signup" className="ml-5 hover:text-neutral-800 dark:hover:text-neutral-50 text-orange-400">
             Sign up!
-          </Link>
+          </Link> : <button onClick={toggleSignUpWarning} className="ml-5 hover:text-neutral-800 dark:hover:text-neutral-50 text-orange-400">
+            Sign up!
+          </button>}
         </Form>
+        <div id="signUpWarning" className={showSignUpWarning ? 'py-1 px-2 mt-4 my-10' : 'hidden'}>
+          <p>You are offline - to sign up, please make sure you have an internet connection to continue.</p>
+        </div>
       </div>
     );
   }
@@ -45,38 +73,40 @@ export default function Index() {
 export async function action({ request }) {
   console.log(request);
   try {
-      const session = await getSession(request.headers.get("Cookie"));
-      const db = await connectDb();
-      const form = await request.formData();
-      let data = "";
+    const session = await getSession(request.headers.get("Cookie"));
+    const db = await connectDb();
+    const form = await request.formData();
+    let data = "";
 
-      const user = await db.models.user.findOne({
-        username: form.get("username"),
-      });
-      if (user != null) {
-        const isCorrectPassword = await bcrypt.compare(form.get("password"), user.password);
+    const user = await db.models.user.findOne({
+      username: form.get("username"),
+    });
+    if (user != null) {
+      const isCorrectPassword = await bcrypt.compare(form.get("password"), user.password);
 
-        if (user != null && isCorrectPassword == true) {
-          session.set("userID", user._id);
-          return redirect("/snippets", {
-            headers: {
-              "Set-Cookie": await commitSession(session),
-            },
-          });
-        }
-        else {
-          data = "wrong login or password";
-          return data;
-        }
+      if (user != null && isCorrectPassword == true) {
+        session.set("userID", user._id);
+        return redirect("/snippets", {
+          headers: {
+            "Set-Cookie": await commitSession(session),
+          },
+        });
       }
       else {
         data = "wrong login or password";
         return data;
       }
+    }
+    else {
+      data = "wrong login or password";
+      return data;
+    }
   } catch (error) {
     return json(
-      { errors: error.errors, message: "error message",
-       status: 400 }
+      {
+        errors: error.errors, message: "error message",
+        status: 400
+      }
     );
   }
 }
@@ -86,9 +116,9 @@ export function ErrorBoundary({ error }) {
     <div className='grid grid-cols-1 bg-slate-900 p-4 rounded-lg shadow-lg mt-5 space-y-10'>
       <h1>You are already logged in :)</h1>
       <b>We could merely prevent this from happening but I want to show this error that occurs</b>
-      <div className='px-10 animate-pulse transition delay-300'> 
-         <h1 className="text-white font-bold">
-            {error.name}: {error.message}
+      <div className='px-10 animate-pulse transition delay-300'>
+        <h1 className="text-white font-bold">
+          {error.name}: {error.message}
         </h1>
       </div>
       <Link to="/" className="py-1 px-4 border-2 
@@ -106,14 +136,14 @@ export function CatchBoundary() {
     <div className='grid grid-cols-1 bg-slate-900 p-4 rounded-lg shadow-lg mt-5 space-y-10'>
       <h3>Sorry, you cannot login while offline</h3>
       <p>Please make sure you have an internet connection and try again.</p>
-      <div className='px-10 animate-pulse transition delay-300'> 
+      <div className='px-10 animate-pulse transition delay-300'>
         <h1>
           {caught.status} {caught.statusText}
         </h1>
         <h2><b>{caught.data}</b></h2>
       </div>
       <Link to="/" className="ml-3 transition hover:bg-slate-500 bg-slate-600 p-4 rounded-lg">
-          Return to Home Page :)
+        Return to Home Page :)
       </Link>
     </div>
   );
--- app/routes/snippets/$snipId/index.jsx
+++ app/routes/snippets/$snipId/index.jsx
@@ -13,18 +13,49 @@ export async function loader({ params, request }) {
       status: 404,
     });
   }
-  return json(snippet);
+  const snippetResponseObject = {
+    currentSnippet: snippet,
+    url: request.url
+  }
+  return json(snippetResponseObject);
 }
 
-
 export default function SnipPage() {
-  const snip = useLoaderData();
+  const snippetData = useLoaderData();
   const [show, setShow] = useState(null);
   const [sort, setSort] = useState("");
+  const [networkState, setNetworkState] = useState();
+  const [shareSnippet, setShareSnippet] = useState(false);
+
+  // I had to pass the url somehow
+  const snip = snippetData.currentSnippet;
+  const snippetURL = snippetData.url;
+  // Add Tags
+  const toggleShareSnippet = (e) => {
+    e.preventDefault();
+    setShareSnippet(!shareSnippet);
+  }
+
+  // Copy to clipboard function
+  const copySnippetURL = (e) => {
+    e.preventDefault();
+    /* Get the text field */
+    var copyText = document.getElementById("snippetURL");
+
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.value);
+
+    /* Alert the copied text */
+    alert("Copied the snippet url: " + copyText.value);
+  }
 
   return (
     <div key={snip._id} id={snip._id} className="grid grid-cols-1 p-2">
-      <div id="Snippet-Data" className='grid grid-cols-1 space-y-2 '>
+      <div id="Snippet-Data" className='grid grid-cols-1 space-y-2 mb-5'>
         <h1 className="text-2xl font-bold mb-2">{snip.title}</h1>
         <h1>
           <b>Date:</b> {snip.date}
@@ -48,8 +79,19 @@ export default function SnipPage() {
           <b>Favorite:</b> {snip.favorite ? 'Yes' : 'No'}
         </h1>
       </div>
+      {
+        shareSnippet ? <div className='grid grid-cols-1 space-y-4 bg-neutral-900 p-4 border-2 border-orange-400 rounded-lg text-neutral-50'>
+          <h1>Share your code snippet</h1>
+          <input className='bg-neutral-900 p-2 border-2 border-orange-300 rounded-lg text-neutral-50' id="snippetURL" defaultValue={snippetURL} />
 
-      <div id="Snippet-Form-Handler" className='grid grid-cols-3 justify-items-center mt-4'>
+          <button className="py-1 px-4 border-2 
+                  border-orange-400 bg-neutral-900 text-neutral-50 rounded-3xl
+                  hover:bg-orange-400" onClick={copySnippetURL}>Copy link</button>
+          <input placeholder="email" type="text" />
+        </div> : <></>
+      }
+
+      <div id="Snippet-Form-Handler" className='grid grid-cols-4 space-x-2 justify-items-center mt-5'>
         {/* Deleting post method with Form and POST */}
         <form method="post" action={useFormAction("delete")}>
           <input type="hidden" name="inputID" value={snip._id}></input>
@@ -67,12 +109,17 @@ export default function SnipPage() {
                   hover:bg-orange-400">Favorite</button>
         </form>
 
-
+        {/* Share Snippet post method with passing ID in the link */}
+        <button type="submit" className="py-1 px-4 border-2 
+                  border-orange-400 bg-neutral-800 text-neutral-50 rounded-3xl
+                  hover:bg-orange-400" onClick={toggleShareSnippet}>{shareSnippet ? "Close" : "Share"}</button>
 
         {/* Editing post method with passing ID in the link */}
         <Link to="update" className="py-1 px-4 border-2 
                   border-orange-400 bg-neutral-800 text-neutral-50 rounded-3xl
                   hover:bg-orange-400">Update</Link>
+
+
       </div>
     </div>
   )
@@ -83,14 +130,14 @@ export function CatchBoundary() {
   return (
     <div className='grid grid-cols-1 bg-slate-900 p-4 rounded-lg shadow-lg mt-5 space-y-10'>
       <h3>Whoopsies</h3>
-      <div className='px-10 animate-pulse transition delay-300'> 
+      <div className='px-10 animate-pulse transition delay-300'>
         <h1>
           {caught.status} {caught.statusText}
         </h1>
         <h2><b>{caught.data}</b></h2>
       </div>
       <Link to="/" className="ml-3 transition hover:bg-slate-500 bg-slate-600 p-4 rounded-lg">
-          Return to Home Page :)
+        Return to Home Page :)
       </Link>
     </div>
   );
@@ -98,12 +145,12 @@ export function CatchBoundary() {
 
 export function ErrorBoundary({ error }) {
   return (
-   
+
     <div className='grid grid-cols-1 bg-neutral-900 p-4 rounded-lg shadow-lg mx-5 space-y-10'>
       <h3>Oh no, seems like we couldn't find that snippet:</h3>
-      <div className='px-10 animate-pulse transition delay-300'> 
-         <p className="text-white font-bold">
-            {error.name}: {error.message}
+      <div className='px-10 animate-pulse transition delay-300'>
+        <p className="text-white font-bold">
+          {error.name}: {error.message}
         </p>
       </div>
       <Link to="/" className="py-1 px-4 border-2 
