fac3aee95373ebd3d8741d826487d5228713245f
Penda Svane <thesvaneman@gmail.com>
2022-06-29 07:16:39 +0200

    v.1.0.6
    -> Fixed critical error with seed.jsx
    -> Updated seed.json

--- app/routes/snippets/seed.jsx
+++ app/routes/snippets/seed.jsx
@@ -1,6 +1,6 @@
 import { useLoaderData, json, Link, Form, redirect, useCatch } from "remix";
 import connectDb from "~/db/connectDb.server";
-import { requireUserSession } from "~/sessions.server";
+import { getSession, requireUserSession } from "~/sessions.server";
 import snippetSeed from "~/db/seed.json";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
@@ -14,22 +14,27 @@ export async function loader({ request }) {
 
 export async function action({ request }) {
   const form = await request.formData();
-  const params = form._fields;
-  console.log(params);
+  const snippetCount = form.get("snippetCount");
   const db = await connectDb();
   try {
+    await requireUserSession(request);
+    const session = await getSession(request.headers.get("Cookie"));
+    const userID = session.get("userID");
     // Delete all Snippets on Database if not already empty
-    if (params.snippetCount.toString() !== "0") {
+    if (snippetCount !== 0) {
       const deletedManySnippets = await db.models.snip.deleteMany({ user: userID });
-      // Inset Default Seed to Database
-      const insertDefaultSeedSnippets = await db.models.snip.insertMany(
-        snippetSeed
-      );
+      // Inset Default Seed to Database -> Data pumping to add more data
+
+      snippetSeed.forEach((snippetData, i) => {
+        CreateDefaultSnippets(db, snippetData, userID, i);
+      });
+
       return redirect("/snippets");
     } else {
-      const insertDefaultSeedSnippets = await db.models.snip.insertMany(
-        snippetSeed
-      );
+      snippetSeed.forEach((snippetData, i) => {
+        CreateDefaultSnippets(db, snippetData, userID, i);
+      });
+
       return redirect("/snippets");
     }
   } catch (error) {
@@ -38,6 +43,11 @@ export async function action({ request }) {
       { status: 400 }
     );
   }
+}
+
+async function CreateDefaultSnippets(db, snippetData, userID, i) {
+  const snipTitle = snippetData.title;
+  let defaultSnippet = await db.models.snip.create({ title: snipTitle, language: snippetData.language, code: snippetData.code, description: snippetData.description, tags: snippetData.tags, favorite: snippetData.favorite, shareable: snippetData.shareable, user: userID.toString() });
 }
 
 export default function Index() {
@@ -49,35 +59,15 @@ export default function Index() {
   }, []);
   return (
     <Form method="post" className="p-4 lg:col-span-3" name="seedForm">
-      <h1 className="text-2xl mb-5">Seeding the database</h1>
-      <h2 className="text-xl mb-10">
-        You currently have <b>{snippetCount}</b> snipppets in your database.
-      </h2>
-      {snippetCount === 0 ? (
-        <>
-          <p className="mb-4 text-lg">
-            Would you like to repopulate your database with{" "}
-            <b>{snippetJSON.length}</b> default snippets?
-          </p>
-        </>
-      ) : (
-        <>
-          <p className="mb-4 text-lg">
-            Do you want to delete them and re-seed the database with{" "}
-            <b>{snippetJSON.length}</b> default snippets?
-          </p>
-          <p id="blackBox" className="text-red-500 bg-black rounded-lg p-4">
-            You are about to reseed your database, are you sure you want to
-            continue? This action is irreversible.
-          </p>
-        </>
-      )}
+      <ReseedBanner snippetCount={snippetCount} />
+      <ConditionalReseedBanner snippetCount={snippetCount} snippetJSON={snippetJSON} />
       <div id="seed-options" className="grid grid-cols-2 mt-5">
         <input
-          type="hidden"
+          type="text"
           id="snippetCount"
           name="snippetCount"
           defaultValue={snippetCount}
+          className="hidden"
         />
         {/* <input type="submit" name="acceptSeed" id="acceptSeed" value="Accept" className="text-white transition hover:bg-red-600 bg-red-800 m-4 rounded-lg h-10" /> */}
         <button
@@ -113,12 +103,50 @@ export default function Index() {
   );
 }
 
+function ReseedBanner({ snippetCount }) {
+  return (
+    <>
+      <h1 className="text-2xl mb-5">Seeding the database</h1>
+      <h2 className="text-xl mb-10">
+        You currently have <b>{snippetCount}</b> snipppets in your database.
+      </h2>
+    </>
+  )
+}
+
+function ConditionalReseedBanner({ snippetCount, snippetJSON }) {
+  return (
+    <>
+      {snippetCount === 0 ? (
+        <>
+          <p className="mb-4 text-lg">
+            Would you like to repopulate your database with{" "}
+            <b>{snippetJSON.length}</b> default snippets?
+          </p>
+        </>
+      ) : (
+        <>
+          <p className="mb-4 text-lg">
+            Do you want to delete them and re-seed the database with{" "}
+            <b>{snippetJSON.length}</b> default snippets?
+          </p>
+          <p id="blackBox" className="text-red-500 bg-black rounded-lg p-4">
+            You are about to reseed your database, are you sure you want to
+            continue? This action is irreversible.
+          </p>
+        </>
+      )}
+    </>
+  )
+}
+
+
 export function CatchBoundary() {
   const caught = useCatch();
   return (
     <div>
       <h1>
-        {caught.status} {caught.statusText}
+        {caught.status} {caught.statusText}
       </h1>
       <h2>{caught.data}</h2>
     </div>
